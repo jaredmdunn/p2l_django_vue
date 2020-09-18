22c1ef3694f4cb5be2e0e5e3c6f64731c7c39726
corey <corey_wood@brown.edu>
2020-09-18 10:38:50 -0400

    Add name indicator to pages urls

--- pages/urls.py
+++ pages/urls.py
@@ -4,7 +4,7 @@ from .views import AboutUsView, ContactUsView, HomePageView
 
 app_name = 'pages'
 urlpatterns = [
-    path('about/', AboutUsView.as_view(), 'about-us'),
-    path('contact/', ContactUsView.as_view(), 'contact-us'),
-    path('', HomePageView.as_view(), 'homepage'),
+    path('about/', AboutUsView.as_view(), name='about-us'),
+    path('contact/', ContactUsView.as_view(), name='contact-us'),
+    path('', HomePageView.as_view(), name='homepage'),
 ]
