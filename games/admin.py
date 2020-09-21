7b58c9708ce6535e77d32974145d6837d862ec34
Jared Dunn <jdunn@webucator.com>
2020-09-21 14:07:50 -0400

    add inline admin capabilities

--- games/admin.py
+++ games/admin.py
@@ -1,27 +1,30 @@
 from django.contrib import admin
 from .models import Game, Parameter, GameScore, GameScoreParameters
 
+class ParameterInline(admin.TabularInline):
+    model = Parameter
+    list_display = ['parameter','slug','game_id', 'values']
+
+
 @admin.register(Game)
 class GameAdmin(admin.ModelAdmin):
     model = Game
     list_display = ['game']
+    inlines = [ParameterInline]
 
     def get_readonly_fields(self, request, obj=None):
         if obj:
             return('slug',)
         return ()
 
-@admin.register(Parameter)
-class ParameterAdmin(admin.ModelAdmin):
-    model = Parameter
-    list_display = ['parameter','slug','game_id', 'values']
+
+class GameScoreParametersInline(admin.TabularInline):
+    model = GameScoreParameters
+    list_display = ['gamescore_id','parameter_id','value']
+
 
 @admin.register(GameScore)
 class GameScoreAdmin(admin.ModelAdmin):
     model = GameScore
     list_display = ['user_id','game_id','score','created']
-
-@admin.register(GameScoreParameters)
-class GameScoreParametersAdmin(admin.ModelAdmin):
-    model = GameScoreParameters
-    list_display = ['gamescore_id','parameter_id','value']
+    inlines = [GameScoreParametersInline]
