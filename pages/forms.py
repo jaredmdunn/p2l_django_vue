84d4cd714a33d51fb47ed72c0fce4ff725ea75f9
Jared Dunn <jdunn@webucator.com>
2020-10-07 12:29:48 -0400

    Make contact form functional

---
+++ pages/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+
+class ContactUsForm(forms.Form):
+    first_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'First Name'})
+    )
+    last_name = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Last Name'})
+    )
+    email = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'e.g., name@example.com'})
+    )
+    subject = forms.CharField(
+        widget=forms.TextInput(attrs={'placeholder': 'Subject'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(
+            attrs={'cols': '100', 'rows': '5', 'placeholder': 'Message'})
+    )
--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import AboutUsView, ContactUsView, HomePageView, VuePageView
+from .views import AboutUsView, ContactUsView, HomePageView, ContactUsThanksView
 
 
 app_name = 'pages'
 urlpatterns = [
     path('about/', AboutUsView.as_view(), name='about-us'),
     path('contact/', ContactUsView.as_view(), name='contact-us'),
-    path('vue', VuePageView.as_view(), name='vue-page'),
+    path('contact/thanks', ContactUsThanksView.as_view(), name='thanks'),
     path('', HomePageView.as_view(), name='homepage'),
 ]
